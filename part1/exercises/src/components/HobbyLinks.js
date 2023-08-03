b2d2ef820f8b305d3cdbf426df0af45ade233e65
Jesse Lawson <jesse.t.lawson@gmail.com>
2023-08-03 15:53:58 -0500

    Added hobbylink files and finished part1 will push to github and submit to lauchcode.

---
+++ part1/exercises/src/components/HobbyLinks.js
@@ -0,0 +1,16 @@
+
+
+let HobbyLinks = function (){
+    let hobbyLinks = ["https://ageofminiatures.com/miniature-painting-techniques/","https://www.thecrucible.org/guides/blacksmithing/"];
+
+    return (
+        <>
+        
+        <a href = {hobbyLinks[0]}>Miniatures</a>
+        <a href = {hobbyLinks[1]}>Blacksmithing</a>
+    
+        
+        </>
+    );
+}
+export default HobbyLinks;
