80fe0b21605a37115580c1f0baf1f1407895a2ca
Jesse Lawson <jesse.t.lawson@gmail.com>
2023-08-09 19:11:44 -0500

    added info to recipe name/image/ingredients

--- part2/studio/src/components/RecipeImage.js
+++ part2/studio/src/components/RecipeImage.js
@@ -1,8 +1,18 @@
+import recipedata from "./recipe.json"
+
 function RecipeImage() {
-   return;
- }
+  let recipeImage = recipedata.map(recipe => {
+    
+    return <img key={recipe.name} src={recipe.recipeImage} alt={recipe.name} className="recipeImage"/>
+  });   
+  
+  return <div>{recipeImage}</div>;
+}
  
- export default RecipeImage;
+
+
+
+export default RecipeImage;
  
  //import json file for the data
  //apply css for className recipeImage
